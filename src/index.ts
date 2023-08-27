86c8e8036c8dc5fdf74fa4e491eb3497e804dad1
Бортников Никита <nikita.bortnikov@gazprombank.ru>
2023-08-27 19:41:28 +0400

    binance connector

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import { config } from "./config";
+import { Spot } from "@binance/connector";
 
-console.log("test");
+const apiKey = "";
+const apiSecret = "";
+const client = new Spot(apiKey, apiSecret);
+
+// Get account information
+client.account().then((response) => client.logger.log(response.data));
+
+// Place a new order
+// client.newOrder('BNBUSDT', 'BUY', 'LIMIT', {
+//   price: '350',
+//   quantity: 1,
+//   timeInForce: 'GTC'
+// }).then(response => client.logger.log(response.data))
+//   .catch(error => client.logger.error(error))
