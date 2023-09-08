d42d3566ea51a174fed80d7fa231022a597b79d4
Бортников Никита <nikita.bortnikov@gazprombank.ru>
2023-09-08 22:36:14 +0400

    import order type

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,42 +2,37 @@ import { config } from "./config";
 import { Spot } from "@binance/connector";
 import { WebsocketStream } from "@binance/connector";
 import * as fs from "fs";
+import { OrderExecutionReport } from "./types";
 
 const { Console } = console;
-const websocketOut = fs.createWriteStream('./logs/websocket/out.log');
-const websocketErr = fs.createWriteStream('./logs/websocket/err.log');
+const websocketOut = fs.createWriteStream("./logs/websocket/out.log");
+const websocketErr = fs.createWriteStream("./logs/websocket/err.log");
 
 const donorApiKey = config.binanceApiKey;
 const donorApiSecret = config.binanceApiKey;
 
 const client = new Spot(donorApiKey);
 
-(async function(){
+(async function () {
+  const listenKey = (await client.createListenKey()).data.listenKey;
 
-    const listenKey = (await client.createListenKey()).data.listenKey;
+  const logger = new Console({ stdout: websocketOut, stderr: websocketErr });
 
-    const logger = new Console({ stdout: websocketOut, stderr: websocketErr });
+  const callbacks = {
+    open: () => logger.debug("Connected with Websocket server"),
+    close: () => logger.debug("Disconnected with Websocket server"),
+    message: (data: OrderExecutionReport) => logger.info(data),
+  };
 
-    const callbacks = {
-        open: () => logger.debug('Connected with Websocket server'),
-        close: () => logger.debug('Disconnected with Websocket server'),
-        message: data => logger.info(data)
-    }
+  const websocketStreamClient = new WebsocketStream({ logger, callbacks });
+  websocketStreamClient.userData(listenKey);
 
-    const websocketStreamClient = new WebsocketStream({ logger, callbacks });
-    websocketStreamClient.userData(listenKey);
-
-    setTimeout(() => websocketStreamClient.disconnect(), 600000);
-
+  setTimeout(() => websocketStreamClient.disconnect(), 600000);
 })();
-
-
-
 
 //const apiKey = config.binanceApiKey;
 //const apiSecret = config.binanceApiSecretKey;
 //const client = new Spot(apiKey, apiSecret);
-
 
 // Get account information
 //client.account().then((response) => client.logger.log(response.data));
---
+++ api/src/types.ts
@@ -0,0 +1,171 @@
+export interface OrderExecutionReport {
+  /**
+   * Event type (e.g., "executionReport")
+   */
+  e: string;
+
+  /**
+   * Event time (e.g., 1499405658658)
+   */
+  E: number;
+
+  /**
+   * Symbol (e.g., "ETHBTC")
+   */
+  s: string;
+
+  /**
+   * Client order ID (e.g., "mUvoqJxFIILMdfAW5iGSOW")
+   */
+  c: string;
+
+  /**
+   * Side (e.g., "BUY")
+   */
+  S: string;
+
+  /**
+   * Order type (e.g., "LIMIT")
+   */
+  o: string;
+
+  /**
+   * Time in force (e.g., "GTC")
+   */
+  f: string;
+
+  /**
+   * Order quantity (e.g., "1.00000000")
+   */
+  q: string;
+
+  /**
+   * Order price (e.g., "0.10264410")
+   */
+  p: string;
+
+  /**
+   * Stop price (e.g., "0.00000000")
+   */
+  P: string;
+
+  /**
+   * Iceberg quantity (e.g., "0.00000000")
+   */
+  F: string;
+
+  /**
+   * OrderListId (e.g., -1)
+   */
+  g: number;
+
+  /**
+   * Original client order ID; This is the ID of the order being canceled (e.g., "")
+   */
+  C: string;
+
+  /**
+   * Current execution type (e.g., "NEW")
+   */
+  x: string;
+
+  /**
+   * Current order status (e.g., "NEW")
+   */
+  X: string;
+
+  /**
+   * Order reject reason; will be an error code (e.g., "NONE")
+   */
+  r: string;
+
+  /**
+   * Order ID (e.g., 4293153)
+   */
+  i: number;
+
+  /**
+   * Last executed quantity (e.g., "0.00000000")
+   */
+  l: string;
+
+  /**
+   * Cumulative filled quantity (e.g., "0.00000000")
+   */
+  z: string;
+
+  /**
+   * Last executed price (e.g., "0.00000000")
+   */
+  L: string;
+
+  /**
+   * Commission amount (e.g., "0")
+   */
+  n: string;
+
+  /**
+   * Commission asset (e.g., null)
+   */
+  N: null;
+
+  /**
+   * Transaction time (e.g., 1499405658657)
+   */
+  T: number;
+
+  /**
+   * Trade ID (e.g., -1)
+   */
+  t: number;
+
+  /**
+   * Ignore (e.g., 8641984)
+   */
+  I: number;
+
+  /**
+   * Is the order on the book? (e.g., true)
+   */
+  w: boolean;
+
+  /**
+   * Is this trade the maker side? (e.g., false)
+   */
+  m: boolean;
+
+  /**
+   * Ignore (e.g., false)
+   */
+  M: boolean;
+
+  /**
+   * Order creation time (e.g., 1499405658657)
+   */
+  O: number;
+
+  /**
+   * Cumulative quote asset transacted quantity (e.g., "0.00000000")
+   */
+  Z: string;
+
+  /**
+   * Last quote asset transacted quantity (e.g., "0.00000000")
+   */
+  Y: string;
+
+  /**
+   * Quote Order Quantity (e.g., "0.00000000")
+   */
+  Q: string;
+
+  /**
+   * Working Time; This is only visible if the order has been placed on the book. (e.g., 1499405658657)
+   */
+  W: number;
+
+  /**
+   * selfTradePreventionMode (e.g., "NONE")
+   */
+  V: string;
+}
